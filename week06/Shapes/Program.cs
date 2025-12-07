0c6f0ff7cbf96f5d8c97195d57590ba213343e1e
MaxTidwell <mtidwell@byupathway.edu>
2025-12-06 22:28:15 -0700

    shapes activity

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,62 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Red", 3);
+        shapes.Add(s1);
+
+        Rectangle s2 = new Rectangle("Blue", 4, 5);
+        shapes.Add(s2);
+
+        Circle s3 = new Circle("Green", 6);
+        shapes.Add(s3);
+
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
+
+
+/*
+example 
+
+// the parent class showing the abstract keyword included
+public abstract class Employee
+{
+  private string _employeeName;
+
+  // Notice the abstract method doesn't have a body at all (not even an empty one)
+  // and it is followed by a semicolon.
+  public abstract float CalculatePay();
+}
+
+// a child class
+public class SalaryEmployee : Employee
+{
+  private float salary = 100f;
+
+  public override float CalculatePay()
+  {
+    return salary;
+  }
+}
+
+// a child class
+public class HourlyEmployee : Employee
+{
+  private float rate = 9f;
+  private float hours = 100f;
+
+  public override float CalculatePay()
+  {
+    return rate * hours; // pay is calculated differently
+  }
+}
+
+*/
